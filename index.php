2ffcef46881e9335d2f5b12c992eb92f4ee3dfe8
rhivent <zzzzjrjr@gmail.com>
2019-03-20 17:20:04 +0700

    update data #2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ $hari	= date("d");
 $bulan	= date ("m");
 $tahun	= date("Y");
 $jumlahhari=date("t",mktime(0,0,0,$bulan,$hari,$tahun));
-var_dump($jumlahhari);
+// var_dump($jumlahhari);
 ?>
 <table style="border:2px solid #1E90FF">
   <tr bgcolor="#ADD8E6">
@@ -17,26 +17,26 @@ var_dump($jumlahhari);
   </tr>
   <?php
 $s=date ("w", mktime (0,0,0,$bulan,1,$tahun));
- var_dump($s);
+ // var_dump($s);
 for ($ds=1;$ds<=$s;$ds++) {
 echo "<td></td>";
 }
- 
+
 for ($d=1;$d<=$jumlahhari;$d++) {
- 
+
 	if (date("w",mktime (0,0,0,$bulan,$d,$tahun)) == 0) {
-		echo "<tr>"; 
+		echo "<tr>";
 		}
 	$warna="#000000"; // warna default
 	$warna_now="#990099"; // warna tgl skrng
 	 if($hari== date('d')){echo "";}
 	if (date("l",mktime (0,0,0,$bulan,$d,$tahun)) == "Sunday") { $warna="#FF0000"; }
 	if($d == date('d')){
-		echo "<td align=center valign=middle style='background-color:$warna_now;'> <span style=\"color:#fff\"><b>$d</b></span></td>"; 
+		echo "<td align=center valign=middle style='background-color:$warna_now;'> <span style=\"color:#fff\"><b>$d</b></span></td>";
 	}else{
-		echo "<td align=center valign=middle> <span style=\"color:$warna\">$d</span></td>"; 
+		echo "<td align=center valign=middle> <span style=\"color:$warna\">$d</span></td>";
 	}
 	if (date("w",mktime (0,0,0,$bulan,$d,$tahun)) == 6) { echo "</tr>"; }
 }
-echo '</table>'; 
+echo '</table>';
 ?>
